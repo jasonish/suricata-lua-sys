e929ad4e3c713bd7f2925b9b9e8fe1a426e250ba
Jason Ish <jish@oisf.net>
2024-04-14 12:20:20 -0600

    lua: build and expose for linkage

---
+++ build.rs
@@ -0,0 +1,40 @@
+use std::{env, path::PathBuf, process::Command};
+
+fn main() {
+    let host = env::var_os("HOST")
+        .map(|host| host.into_string().unwrap())
+        .unwrap();
+
+    let out_dir = env::var_os("OUT_DIR").map(PathBuf::from).unwrap();
+    let build_dir = out_dir.join("lua");
+
+    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR")
+        .map(PathBuf::from)
+        .unwrap();
+
+    // Copy the source into a build directory as we shouldn't build in the
+    // source directory.
+    let _ = std::fs::remove_dir_all(&build_dir);
+    std::fs::create_dir_all(&out_dir).unwrap();
+    let opts = fs_extra::dir::CopyOptions {
+        copy_inside: true,
+        ..Default::default()
+    };
+    fs_extra::dir::copy(manifest_dir.join("lua"), &build_dir, &opts).unwrap();
+
+    let mut command = Command::new("make");
+    if host.contains("windows") {
+        command.arg("mingw");
+    }
+    command.arg("MYCFLAGS=-fPIC");
+
+    let status = command
+        .current_dir(&build_dir).status().unwrap();
+    let status = command.current_dir(&build_dir).status().unwrap();
+    if !status.success() {
+        panic!("build failed");
+    }
+
+    println!("cargo:rustc-link-lib=static=lua");
+    println!("cargo:rustc-link-search=native={}", build_dir.display());
+}
