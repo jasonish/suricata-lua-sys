cb59f91de7110991b2c68f58db8b01a7c22c9b9b
Jason Ish <jish@oisf.net>
2025-06-11 14:33:14 -0600

    build: revert back to default make
    This is smarter as on Linux it sets flags for Linux such as enabling
    POSIX which gets rid of the tmpnam warning.
    
    Update version to 5.4.8001.

--- build.rs
+++ build.rs
@@ -40,9 +40,6 @@ fn main() {
         env::var("SURICATA_LUA_SYS_CFLAGS").unwrap_or_default()
     ));
 
-    // We only want the library, not the tool.
-    command.arg("a");
-
     // Don't inherit parent MAKEFLAGS, they may not be suitable for
     // this build.
     command.env_remove("MAKEFLAGS");
